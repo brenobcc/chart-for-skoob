c09a89bc1a6c3c47f165f43bc3b057d91a423c1e
brenobcc <breno.cvcr@gmail.com>
2024-12-30 16:07:43 -0300

    refactor(src): replacing the list of bytes with a dictionary

--- src/last_read_books.py
+++ src/last_read_books.py
@@ -21,13 +21,14 @@ if response.status_code == 200:
     # with open("logs/log.json", "w", encoding="utf-8") as file:
     #     json.dump(response_json, file, indent=4, ensure_ascii=False)
 
-    n = 12
+    columns, lines = map(int, input("Selecione o tamanho do grid:").split())
+    books_quantity = columns * lines
 
     try:
-        chart_imgs = []
+        chart_imgs = {}
 
         # Algoritmo para varrer JSON
-        for i in range(n):
+        for i in range(books_quantity):
             target_element = response_json["response"][i]
             book_edition = target_element["edicao"]
             book_name = book_edition["titulo"]
@@ -41,30 +42,36 @@ if response.status_code == 200:
             new_size = (100, 150)
             book_img_resized = img_book.resize(new_size, Image.Resampling.LANCZOS)
 
+            # Salva dinamicamente a imagem em bytes em um array
             img_byte_value = io.BytesIO()
 
             book_img_resized.save(img_byte_value, format="JPEG")
-            chart_imgs.append(img_byte_value.getvalue())
+            chart_imgs[f"{book_name}"] = img_byte_value.getvalue()
+
+        #Montar grid
+        chart_width = 100 * columns
+        chart_height = 150 * lines
+        new_size = (100, 150)
+
+        book_count = 0
+
+        grid = Image.new("RGB", (chart_width, chart_height), (255, 255, 255))
+        for book, image_bytes in chart_imgs.items():
+
+            if book_count >= 3 * 4:
+                break
+            
+            image = Image.open(io.BytesIO(image_bytes))
+            image = image.convert("RGB")
+
+            x = (book_count % columns) * new_size[0]
+            y = (book_count // columns) * new_size[1]
+
+            grid.paste(image, (x, y))
+
+            book_count += 1
+
+        grid.save("grid4x3.jpg")
     
     except Exception as e:
         print(e)
-
-    #Montar grid
-    largura = 100 * 4
-    altura = 150 * 3
-    new_size = (100, 150)
-
-    grid = Image.new("RGB", (largura, altura), (255, 255, 255))
-    for i, image in enumerate(chart_imgs):
-        if i >= 3 * 4:
-            break
-        
-        image = io.BytesIO(image)
-        image = Image.open(image)
-
-        x = (i % 4) * new_size[0]
-        y = (i // 4) * new_size[1]
-
-        grid.paste(image, (x, y))
-
-    grid.save("grid4x3.jpg")
