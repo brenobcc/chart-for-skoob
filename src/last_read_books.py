fd1ab98143b1f258cac3b8006e273666e82aa98b
brenobcc <breno.cvcr@gmail.com>
2024-12-30 11:48:00 -0300

    feat: saving img using pillow

--- src/last_read_books.py
+++ src/last_read_books.py
@@ -1,6 +1,8 @@
 import requests
 import json
-
+from PIL import Image
+from io import BytesIO
+    
 user_id = input("input your profile id: ")
 
 url = f"https://www.skoob.com.br/v1/bookcase/books/{user_id}/year:2024/page:1/limit:36/"
@@ -21,6 +23,9 @@ if response.status_code == 200:
     n = 5
 
     try:
+        paths = []
+
+        # Algoritmo para varrer JSON
         for i in range(n):
             target_element = response_json["response"][i]
             book_edition = target_element["edicao"]
@@ -29,7 +34,21 @@ if response.status_code == 200:
 
             response_image = requests.get(book_image, headers=headers)
 
-            with open(f"static/images/{book_name}.jpg", "wb") as file:
-                file.write(response_image.content)
+            # BytesIO converte para que Image consiga ler a requisição
+            img_book = Image.open(BytesIO(response_image.content))
+
+            new_size = (100, 150)
+            img_book_resized = img_book.resize(new_size, Image.Resampling.BILINEAR)
+
+            path = f"/img/{book_name}_resized.jpg"
+            img_book_resized.save(path)
+
+            paths.append(path)
+            
+
+            # Salvar na pasta local
+            # with open(f"static/images/{book_name}.jpg", "wb") as file:
+            #     file.write(response_image.content)
+    
     except Exception as e:
         print(e)
