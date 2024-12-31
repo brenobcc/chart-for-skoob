b1b57407b505022751367ffdef9d88d0ac86fac8
brenobcc <breno.cvcr@gmail.com>
2024-12-30 22:08:20 -0300

    feat(src): last read books

--- src/last_read_books.py
+++ src/last_read_books.py
@@ -3,16 +3,27 @@ import json
 from PIL import Image
 import io
 from io import BytesIO
+
+# Colocar todos os livros lidos em uma única página
+def mock_first_page(url, headers, user_id):
+    response = requests.get(url, headers=headers)
+    response_json = response.json()
+    total_books = response_json["paging"]["total"]
+
+    new_url = f"https://www.skoob.com.br/v1/bookcase/books/{user_id}/year:2024/page:1/limit:{total_books}/"
+
+    return new_url
     
 user_id = input("input your profile id: ")
 
-url = f"https://www.skoob.com.br/v1/bookcase/books/{user_id}/year:2024/page:1/limit:36/"
-
+url = f"https://www.skoob.com.br/v1/bookcase/books/{user_id}/year:2024/page:1/limit:1/"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 }
 
-response = requests.get(url, headers=headers)
+new_url = mock_first_page(url, headers, user_id)
+
+response = requests.get(new_url, headers=headers)
 
 print(response.status_code)
 
@@ -22,14 +33,15 @@ if response.status_code == 200:
     #     json.dump(response_json, file, indent=4, ensure_ascii=False)
 
     columns, lines = map(int, input("Selecione o tamanho do grid:").split())
-    books_quantity = columns * lines
+    book_quantity = columns * lines
 
     try:
         chart_imgs = {}
-
+        total_books = response_json["paging"]["total"]
         # Algoritmo para varrer JSON
-        for i in range(books_quantity):
+        for i in range(total_books - 1,  total_books - book_quantity - 1, -1):
             target_element = response_json["response"][i]
+
             book_edition = target_element["edicao"]
             book_name = book_edition["titulo"]
             book_img = book_edition["capa_pequena"]
@@ -58,7 +70,7 @@ if response.status_code == 200:
         grid = Image.new("RGB", (chart_width, chart_height), (255, 255, 255))
         for book, image_bytes in chart_imgs.items():
 
-            if book_count >= 3 * 4:
+            if book_count >= book_quantity:
                 break
             
             image = Image.open(io.BytesIO(image_bytes))
@@ -71,7 +83,7 @@ if response.status_code == 200:
 
             book_count += 1
 
-        grid.save("grid4x3.jpg")
+        grid.save(f"grid{columns}x{lines}.jpg")
     
     except Exception as e:
         print(e)
